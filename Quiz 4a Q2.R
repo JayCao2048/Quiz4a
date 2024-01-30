f22e46b3d7e6ed5c53e656f2543410467472d8ec
JayCao2048 <jayy.cao@mail.utoronto.ca>
2024-01-30 00:49:17 +0000

    Question 2 and Question 4

--- Quiz 4a Q2.R
+++ Quiz 4a Q2.R
@@ -1,3 +1,7 @@
+#install ggplot2 package
+install.packages('ggplot2')
+library(ggplot2)
+
 #Assume that the actual height of the 20 friends follows normal distributionw with mean = 165, s.d. = 15
 actual <- rnorm(20, mean=165, sd=15)
 
@@ -8,7 +12,7 @@ lucy <- actual + rnorm(20, mean=0, sd=6)
 
 #put data into a data frame
 height <- data.frame(Friend=1:20, Acutal_Height=actual, 
-                     Edward_Meansurement = edward, Hugo_Measurement = hugo, 
+                     Edward_Measurement = edward, Hugo_Measurement = hugo, 
                      Lucy_Measurement = lucy)
 
 #Test if there are five variables
@@ -17,3 +21,14 @@ height |> length() == 5
 height$Acutal_Height |> class() == "numeric"
 #Since all height should be positive, test if all actual height are greater than 0.
 height$Acutal_Height |> min() > 0
+
+#Construct the scatter plot
+ggplot() +
+  geom_point(data = height, aes(x = Friend, y = Acutal_Height), color = 'black') +
+  geom_point(data = height, aes(x = Friend, y = Edward_Measurement), color = 'red') +
+  geom_point(data = height, aes(x = Friend, y = Hugo_Measurement), color = 'blue') +
+  geom_point(data = height, aes(x = Friend, y = Lucy_Measurement), color = 'green') +
+  theme_minimal() +
+  labs(title = 'Height Measurements of 20 friends',
+       x = 'Friend',
+       y = 'Height (cm)')
